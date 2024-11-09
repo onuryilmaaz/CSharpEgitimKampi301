3f2b9cf255258d5d7d8fec7bf50563493d49cdda
Onur Yılmaz <Onurylmz.41@outlook.com>
2024-11-09 16:32:54 +0300

    Add project files.

---
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concrete
+{
+	public class Category
+	{
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+    }
+}
+
+//field sınıfın - içinde tanımlanırsa
+//properties - sınıfın içinde tanımlanırsa ve set get alırsa
+//variables - methot içinde tanımlanırsa
